b7650cba5e9dea6e430bb735f889f76b5c6ee415
BJ <bjrodtuazon@gmail.com>
2020-06-30 15:55:50 +0800

    Add forecast additional info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude,longitude,callback) => {
         }else if(body.error){
             callback('Unable to find location. Try another search.', undefined)
         }else{
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' fahrenheit out. It feels like ' + body.current.feelslike + ' fahrenheit out.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' fahrenheit out. It feels like ' + body.current.feelslike + ' fahrenheit out. The wind speed is ' + body.current.wind_speed + '.')
         }
     })
     
